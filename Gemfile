9ce068fad501793eb134a5c837a680b1a1e22230
Shintaro Kojima <goodies@codeout.net>
2023-02-02 20:23:53 +0900

    chore: bundle gem oxidized-mix

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in oxidized-mix.gemspec
+gemspec
+
+gem "rake", "~> 13.0"
---
+++ lib/oxidized/mix.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "mix/version"
+
+module Oxidized
+  module Mix
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
