f64eb07cc6e8ca53ea103568606b29bd24b64ede
Shintaro Kojima <goodies@codeout.net>
2023-02-10 02:22:21 +0900

    refactor(exe): common methods into a module

--- lib/oxidized_mix/oxidized/cli.rb
+++ lib/oxidized_mix/oxidized/cli.rb
@@ -3,22 +3,9 @@ require 'oxidized/cli'
 
 module OxidizedMix
   module Oxidized
-    # Wrapper of Oxidized::CLI to customize
-    class CLI < ::Oxidized::CLI
+    # Util modules
+    module CLIUtil
       attr_reader :extra_config
-
-      def parse_opts
-        # parse once to tweak the default parser
-        _, opts = empty_argv { super }
-
-        result = Slop::Parser.new(
-          custom_options(opts.options),
-          **Slop::Options::DEFAULT_CONFIG
-        ).parse(ARGV)
-        @extra_config = pick_extra_config(result)
-
-        [result.arguments, result]
-      end
 
       private
 
@@ -39,12 +26,6 @@ module OxidizedMix
         opts
       end
 
-      def add_custom_options(opts)
-        opts.string '--git-user', 'git user name to add commits', required: true
-        opts.string '--git-email', 'git user email to add commits', required: true
-        opts.array '--repo', 'repository working directory where oxidized works', required: true
-      end
-
       def pick_extra_config(result)
         config = ::Asetus::ConfigStruct.new
 
@@ -53,6 +34,30 @@ module OxidizedMix
         config.repos = result[:repo]
 
         config
+      end
+    end
+
+    # Wrapper of Oxidized::CLI to customize
+    class CLI < ::Oxidized::CLI
+      include CLIUtil
+
+      def parse_opts
+        # parse once to tweak the default parser
+        _, opts = empty_argv { super }
+
+        result = Slop::Parser.new(
+          custom_options(opts.options),
+          **Slop::Options::DEFAULT_CONFIG
+        ).parse(ARGV)
+        @extra_config = pick_extra_config(result)
+
+        [result.arguments, result]
+      end
+
+      def add_custom_options(opts)
+        opts.string '--git-user', 'git user name to add commits', required: true
+        opts.string '--git-email', 'git user email to add commits', required: true
+        opts.array '--repo', 'repository working directory where oxidized works', required: true
       end
     end
   end
--- lib/oxidized_mix/oxidized/script/cli.rb
+++ lib/oxidized_mix/oxidized/script/cli.rb
@@ -6,7 +6,7 @@ module OxidizedMix
     module Script
       # Wrapper of Oxidized::CLI to customize
       class CLI < ::Oxidized::Script::CLI
-        attr_reader :extra_config
+        include Oxidized::CLIUtil
 
         def opts_parse(cmd)
           # parse once to tweak the default parser
@@ -24,35 +24,8 @@ module OxidizedMix
 
         private
 
-        def empty_argv
-          argv = ARGV.dup
-          ARGV.clear
-          result = yield
-
-          ARGV.replace argv
-          result
-        end
-
-        def custom_options(opts)
-          # disable "--daemonize"
-          opts.options.reject! { |o| o.flag == '--daemonize' }
-
-          add_custom_options opts
-          opts
-        end
-
         def add_custom_options(opts)
           opts.array '--repo', 'repository working directory where oxidized works', required: true
-        end
-
-        def pick_extra_config(result)
-          config = ::Asetus::ConfigStruct.new
-
-          config.output.git.user = result[:git_user]
-          config.output.git.email = result[:git_email]
-          config.repos = result[:repo]
-
-          config
         end
 
         def remove_arguments
