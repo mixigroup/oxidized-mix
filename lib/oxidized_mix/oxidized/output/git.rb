3a843aba5566e4c25157336d0a5c01823d6ec84e
Shintaro Kojima <goodies@codeout.net>
2023-02-16 18:40:19 +0900

    docs: add license statements

--- lib/oxidized_mix/oxidized/output/git.rb
+++ lib/oxidized_mix/oxidized/output/git.rb
@@ -1,3 +1,42 @@
+#--
+# Copyright (c) 2023 MIXI, Inc.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# ---
+#
+# This is derived from material copyright
+#   2013-2015 Saku Ytti <saku@ytti.fi>
+#   2013-2015 Samer Abdel-Hafez <sam@arahant.net>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may not use
+#   this file except in compliance with the License. You may obtain a copy of the
+#   License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software distributed
+#   under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+#   CONDITIONS OF ANY KIND, either express or implied. See the License for the
+#   specific language governing permissions and limitations under the License.
+#++
+
 # rubocop:disable all
 require 'oxidized/output/git'
 
@@ -7,7 +46,7 @@ module OxidizedMix
       # Override the behavior of Oxidized::Git
       # - Store files in sub-directories which are named as group names
       module SubdirStorable
-        # Copy from oxidized-0.28.0/lib/oxidized/output/git.rb in gem, and customize
+        # Modified copy of https://github.com/ytti/oxidized/blob/0.28.0/lib/oxidized/output/git.rb#L143-L169
         def update(repo, file, data)
           return if data.empty?
 
