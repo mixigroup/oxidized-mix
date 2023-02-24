e2a6dbfbe636abd47ca841fe128ab8bd6edb5e53
Shintaro Kojima <goodies@codeout.net>
2023-02-24 20:30:41 +0900

    feat(model): print commands to execute

--- lib/oxidized_mix/oxidized/model.rb
+++ lib/oxidized_mix/oxidized/model.rb
@@ -9,7 +9,16 @@ module OxidizedMix
         super.gsub(/ +\n/, "\n")
       end
     end
+
+    # Override the behavior of Oxidized::Model
+    # - Print commands to execute
+    module CommandPrintable
+      def process_cmd_output(output, name)
+        super.prepend("#{self.class.comment}[ #{name} ]\n")
+      end
+    end
   end
 end
 
 ::Oxidized::Model.prepend OxidizedMix::Oxidized::CommentTrimmable
+::Oxidized::Model.prepend OxidizedMix::Oxidized::CommandPrintable
